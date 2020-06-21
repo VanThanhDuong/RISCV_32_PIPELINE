9a7a1cf2bc822e1df8e4c93cbc0c74e19fa3ca67
VanThanhDuong <66696952+VanThanhDuong@users.noreply.github.com>
2020-06-21 21:01:56 +0700

    IF_ID_STAGE

--- ID_IF_Stage.v
+++ ID_IF_Stage.v
@@ -1,16 +1,8 @@
-module IDIFstage(IFIDwrite,clk,instd_in,pcd_in,pcd_out,instd_out);
+module IDIFstage(rst,en,clk,instd_in,pcd_in,pcd_out,instd_out);
 parameter datawidth = 32;
-input reg clk,IFIDwrite;
+input clk,IFIDWrite,IFIDFlus;
 input [datawidth - 1:0] instd_in, pcd_in;
 output [datawidth - 1:0] instd_out,pcd_out;
-initial 
-begin
-pcd_in <=32'h0;
-instd_in <=32'h0;
-pcd_out <=32'h0;
-instd_out <= 32'h0;
-end
-always@(clk or IFIDwrite)
-begin
-case(IFIDwrite)
-1'b0: 
+p_reg p_regpcd(.rst(rst),.en(en),.clk(clk),.data_in(pcd_in),.data_out(pcd_out));
+p_reg p_reginstd(.rst(rst),.en(en),.clk(clk),.data_in(instd_in),.data_out(instd_out));
+endmodule
